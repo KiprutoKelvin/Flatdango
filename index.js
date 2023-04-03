b469a10cd6e037c21aed9413925fccbeb0726e82
Kiru <kelvin.kipruto@student.moringaschool.com>
2023-04-03 09:02:27 +0300

    adds operators

--- index.js
+++ index.js
@@ -6,3 +6,17 @@ function addNumber(num) {
 	displayValue += num;
 	document.getElementById('display').value = displayValue;
 }
+
+function addOperator(operator) {
+	if (displayValue === '') return;
+
+	const lastChar = displayValue.slice(-1);
+
+	if (lastChar === '+' || lastChar === '-' || lastChar === '*' || lastChar === '/') {
+		displayValue = displayValue.slice(0, -1) + operator;
+	} else {
+		displayValue += operator;
+	}
+
+	document.getElementById('display').value = displayValue;
+}
